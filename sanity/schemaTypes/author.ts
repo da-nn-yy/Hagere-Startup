7ba32c9aa963781ed1de8f9f419057209b1dc75a
da-nn-yy <db7394928@gmail.com>
2025-02-10 18:09:45 +0300

    Add author schema type and index definition for Sanity integration

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,16 @@
+export const author = defineType(schemaDefinition:{
+    name:"author",
+    title:"Author",
+    type:"document"
+    icon: UserIcon,
+    fields:[
+        defineField(schemaField:{
+           name:'id',
+           type:'number'
+        }),
+        defineField(schemaField:{
+            name:'id',
+            type:'number'
+         }),
+    ]
+})
