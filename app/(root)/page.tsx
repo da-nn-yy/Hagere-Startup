5bff707d25f7010023a86d87e3d8d10637290a97
da-nn-yy <db7394928@gmail.com>
2025-02-10 10:21:07 +0300

    Update Home page to include StartupCard component and display search results

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "../../components/StartupCard";
 export default async function Home({searchParams} : {
     searchParams: Promise<{query: string}>} ) {
 
         const query = (await searchParams).query;
-    
+        const posts = [{
+            _createdAt: "Yesterday",
+            views: 100,
+            author: {_id:1},
+            _id:1,
+            description: "A platform for connecting with entrepreneurs",
+            image:"https://images.unsplash.com/photo-1738447429433-69e3ecd0bdd0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            catagory: "Tech",
+            title: "Hagere Startup",
+        }]
+
   return (
     <>
       <section className="heading_container">
@@ -14,6 +25,19 @@ export default async function Home({searchParams} : {
         </p>
 
         <SearchForm query ={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+            {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+            {posts ?. length > 0 ? (
+                posts.map((post:StartupCardType) => (
+                    <StartupCard key={post?._id} post ={post}/>
+            ))):(
+                <p className="no-results">No startups found</p>
+            )}
+        </ul>
       </section>
     </>
   );
