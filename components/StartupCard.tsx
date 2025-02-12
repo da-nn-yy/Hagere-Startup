ed42ec1a90c917c7e718fed79d44e4a2c02d572e
da-nn-yy <db7394928@gmail.com>
2025-02-12 14:35:15 +0300

    Refactor StartupCard component to improve destructuring and handle optional author properties

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,9 +4,19 @@ import {EyeIcon} from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {Button} from '@/components/ui/button'
+import {Startup,Author} from '@/sanity/types'
 
+export type StartupTypeCard = Omit<Startup,"author"> & {author?: Author}
 const StartupCard = ({post}:{post:StartupTypeCard}) => {
-    const {_createdAt,description, author:{_id:authorId,name}, views,title,catagory,_id,image} = post;
+    const  {_createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
   return (
     <li className='startup-card group'>
         <div className="flex-between">
@@ -20,16 +30,16 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
         </div>
         <div className='flex-between mt-5 gap-5'>
             <div>
-                <Link href={`/user/${authorId}`}>
+                <Link href={`/user/${author?._id}`}>
                     <p className='text-16-medium line-clamp-1'>
-                        {name}
+                        {author?.name}
                     </p>
                 </Link>
                 <Link href={`/startup/${_id}`}>
                     <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
                 </Link>
             </div>
-            <Link href={`/user/${authorId}`}>
+            <Link href={`/user/${author?._id}`}>
                 <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className='rounded-full'/>
             </Link>
         </div>
@@ -40,8 +50,8 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
             <img src={image} alt="placeholder" className='startup-card_img'/>
         </Link>
         <div className="flex-between gap-3 mt-5">
-            <Link href={`/query=${catagory.toLowerCase()}`}>
-                <p className='text-16-medium'>{catagory}</p>
+            <Link href={`/query=${category?.toLowerCase()}`}>
+                <p className='text-16-medium'>{category}</p>
             </Link>
             <Button className='startup-card_btn' asChild>
                 <Link href={`/startup/${_id}`}>
