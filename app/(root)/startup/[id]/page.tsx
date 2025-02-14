fc375264b30fbdae1a1bfbe5a34b8b7b5b3319a7
da-nn-yy <db7394928@gmail.com>
2025-02-14 16:24:11 +0300

    Refactor page component to improve readability and update greeting format

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const  page = async ({params }: {params:{id: string}}) => {
-    const id = (await params).id
-  return (
-    <>
-        <h1 className='text-3xl'>This is is a startup page number {id}</h1>
-    </>
-  )
-}
-
-export default page
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const id = (await params).id;
+    return (
+        <>
+            <h1>Hi : {id}</h1>
+        </>
+    );
+};
+export default Page;
