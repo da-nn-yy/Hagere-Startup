b70cc8cb5ab8c75fb8ec1887b22c1567389de7cf
da-nn-yy <db7394928@gmail.com>
2025-02-16 15:40:11 +0300

    Update welcome message to include 'Hagere Startups' for improved branding

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,13 +13,10 @@ export default async function Home({searchParams} : {
 
         const {data : posts} = await sanityFetch({query: STARTUPS_QUERY,params});
 
-
-
-
   return (
     <>
       <section className="heading_container">
-        <p className="tag">Welcome to</p>
+        <p className="tag">Welcome to Hagere Startups</p>
         <h1 className="heading">Pitch your startup,connect with enterepreneurs</h1>
         <p className="sub-heading !max-w-3xl">
             Submit Ideas, Vote on Pitches, and GetNoticed in Virtual Competitions.
