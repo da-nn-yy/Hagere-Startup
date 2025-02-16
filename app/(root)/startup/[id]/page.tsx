182e3c3fc638e15c591cdf858505999ec8313fb6
da-nn-yy <db7394928@gmail.com>
2025-02-16 16:49:13 +0300

    Refactor startup page component to enhance data fetching and rendering logic

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { formatDate } from '@/lib/utils';
+import { client } from '@/sanity/lib/client';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
+import React from 'react';
 
-export const experimatal_ppr = true
-const page = ({params} : {params : Promise<{id : string}> }) => {
+export const experimatal_ppr = true;
 
-  return (
-    <div>page</div>
-  )
-}
+const Page = async ({ params }: { params: { id: string } }) => {
+    
+    const id = params.id;
 
-export default page
+    const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+    console.log({ id });
+
+    if (!post) return notFound();
+
+    return (
+        <>
+            <section className='header-container !min-h-[230px]'>
+                <p className='tag'>{formatDate(post?._createdAt)}</p>
+                <h1 className="heading">{post?.title}</h1>
+                <p className='sub-heading !max-w-5xl'>{post?.description}</p>
+            </section>
+        </>
+    );
+};
+
+export default Page;
